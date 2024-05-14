d8e3d65c8e9cd7b16ceadc8a58d9f045569c613a
Cindy Nguyen <cnguyen5138@gmail.com>
2024-05-14 15:03:55 -0400

    question 3 done

--- src/modify.js
+++ src/modify.js
@@ -11,7 +11,6 @@ const clickCounterHandler = (event) => {
     button.textContent = `I've been clicked ${clicks} times!`
   }
 };
-
 button.addEventListener('click', clickCounterHandler)
 
 const para = document.querySelector('#keydown-tracker')
@@ -21,6 +20,22 @@ const handleKeydown = (event) => {
   para.textContent = `You pressed ${event.code}`
 };
 document.body.addEventListener('keydown', handleKeydown)
+
+
+const inline = document.querySelector('#inline-example')
+
+const clickCounterHandler2 = (event) => {
+  let clicks = Number(inline.getAttribute("data-clicks"))
+  clicks += 1;
+  inline.setAttribute('data-clicks', clicks)
+  console.log(clicks)
+  if (clicks === 1) {
+    inline.textContent = `I've been clicked ${clicks} time.`
+  } else {
+    inline.textContent = `I've been clicked ${clicks} times!`
+  }
+};
+inline.addEventListener('click', clickCounterHandler2)
 
 
 const handleDelegation = (event) => {
